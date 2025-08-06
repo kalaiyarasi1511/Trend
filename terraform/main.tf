701440d281badce2b7637513901a5ac4afd306ea
Ubuntu <ubuntu@ip-172-31-2-60.ap-south-1.compute.internal>
2025-08-06 07:46:07 +0000

    yes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,57 +2,46 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# VPC
 resource "aws_vpc" "trend_vpc" {
   cidr_block           = "10.1.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = {
-    Name = "trend-vpc"
-  }
+  tags = { Name = "trend-vpc" }
 }
 
-# Subnet
 resource "aws_subnet" "trend_subnet" {
   vpc_id                  = aws_vpc.trend_vpc.id
   cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-south-1a"
-  tags = {
-    Name = "trend-subnet"
-  }
+  tags = { Name = "trend-subnet" }
 }
 
-# Internet Gateway
 resource "aws_internet_gateway" "trend_igw" {
   vpc_id = aws_vpc.trend_vpc.id
 }
 
-# Route Table
 resource "aws_route_table" "trend_rtb" {
   vpc_id = aws_vpc.trend_vpc.id
 }
 
-# Route
 resource "aws_route" "trend_route" {
   route_table_id         = aws_route_table.trend_rtb.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.trend_igw.id
 }
 
-# Route Table Association
 resource "aws_route_table_association" "trend_rta" {
   subnet_id      = aws_subnet.trend_subnet.id
   route_table_id = aws_route_table.trend_rtb.id
 }
 
-# Jenkins EC2 Instance
 resource "aws_instance" "jenkins_ec2" {
-  ami           = "ami-0dee22c13ea7a9a67" # Amazon Linux 2023
+  ami           = "ami-0dee22c13ea7a9a67"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.trend_subnet.id
   associate_public_ip_address = true
-  key_name      = "linux-ssh-key" # Your key pair name
+  key_name      = "linux-ssh-key"
 
   user_data = <<-EOF
               #!/bin/bash
@@ -67,7 +56,5 @@ resource "aws_instance" "jenkins_ec2" {
               systemctl enable jenkins
               EOF
 
-  tags = {
-    Name = "trend-jenkins"
-  }
+  tags = { Name = "trend-jenkins" }
 }
