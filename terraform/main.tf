43c6b52440ab72865a62c24787003f8604160448
Ubuntu <ubuntu@ip-172-31-2-60.ap-south-1.compute.internal>
2025-08-06 07:38:27 +0000

    yes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,9 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# VPC
 resource "aws_vpc" "trend_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.1.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
@@ -11,9 +12,10 @@ resource "aws_vpc" "trend_vpc" {
   }
 }
 
+# Subnet
 resource "aws_subnet" "trend_subnet" {
   vpc_id                  = aws_vpc.trend_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-south-1a"
   tags = {
@@ -21,112 +23,36 @@ resource "aws_subnet" "trend_subnet" {
   }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "trend_igw" {
   vpc_id = aws_vpc.trend_vpc.id
 }
 
+# Route Table
 resource "aws_route_table" "trend_rtb" {
   vpc_id = aws_vpc.trend_vpc.id
 }
 
+# Route
 resource "aws_route" "trend_route" {
   route_table_id         = aws_route_table.trend_rtb.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.trend_igw.id
 }
 
+# Route Table Association
 resource "aws_route_table_association" "trend_rta" {
   subnet_id      = aws_subnet.trend_subnet.id
   route_table_id = aws_route_table.trend_rtb.id
 }
 
-# --- Commenting EKS Cluster ---
-# resource "aws_eks_cluster" "trend_cluster" {
-#   name     = "trend-cluster"
-#   role_arn = aws_iam_role.eks_role.arn
-#   version  = "1.29"
-#
-#   vpc_config {
-#     subnet_ids = [aws_subnet.trend_subnet.id]
-#   }
-#
-#   depends_on = [aws_iam_role_policy_attachment.eks_policy_attach]
-# }
-
-resource "aws_iam_role" "eks_role" {
-  name = "trend-eks-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_policy_attach" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role" "node_role" {
-  name = "trend-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
-  role       = aws_iam_role.node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-# --- Commenting EKS Node Group ---
-# resource "aws_eks_node_group" "trend_nodes" {
-#   cluster_name    = aws_eks_cluster.trend_cluster.name
-#   node_group_name = "trend-nodes"
-#   node_role_arn   = aws_iam_role.node_role.arn
-#   subnet_ids      = [aws_subnet.trend_subnet.id]
-#   scaling_config {
-#     desired_size = 2
-#     max_size     = 2
-#     min_size     = 1
-#   }
-#   depends_on = [
-#     aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
-#     aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
-#     aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy
-#   ]
-# }
-
+# Jenkins EC2 Instance
 resource "aws_instance" "jenkins_ec2" {
-  ami           = "ami-0dee22c13ea7a9a67" # Amazon Linux 2023 (ap-south-1)
+  ami           = "ami-0dee22c13ea7a9a67" # Amazon Linux 2023
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.trend_subnet.id
   associate_public_ip_address = true
-  key_name      = "linux-ssh-key"
+  key_name      = "linux-ssh-key" # Your key pair name
 
   user_data = <<-EOF
               #!/bin/bash
